672ab3e7db8e86a4513b9320ea4a9d2a686a893e
Ashish sharma <sharma07101998@gmail.com>
2023-04-14 15:50:31 +0530

    completed sidebar of ui

---
+++ src/components/sideBar/SideBar.jsx
@@ -0,0 +1,95 @@
+import { RssFeed, Chat } from "@mui/icons-material";
+import Video from "@mui/icons-material/PlayCircle";
+import Group from "@mui/icons-material/People";
+import Bookmark from "@mui/icons-material/Bookmark";
+import Questions from "@mui/icons-material/HelpOutline";
+import Job from "@mui/icons-material/WorkOutlineOutlined";
+import Event from "@mui/icons-material/EventOutlined";
+import Course from "@mui/icons-material/SchoolOutlined";
+import React from "react";
+import "./SideBar.css";
+
+function SideBar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <RssFeed className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <Chat className="sidebarIcon" />
+            <span className="sidebarListItemText">Chat</span>
+          </li>
+          <li className="sidebarListItem">
+            <Video className="sidebarIcon" />
+            <span className="sidebarListItemText">Video</span>
+          </li>
+          <li className="sidebarListItem">
+            <Group className="sidebarIcon" />
+            <span className="sidebarListItemText">Group</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidebarListItemText">Bookmark</span>
+          </li>
+          <li className="sidebarListItem">
+            <Questions className="sidebarIcon" />
+            <span className="sidebarListItemText">Question</span>
+          </li>
+          <li className="sidebarListItem">
+            <Job className="sidebarIcon" />
+            <span className="sidebarListItemText">Job</span>
+          </li>
+          <li className="sidebarListItem">
+            <Event className="sidebarIcon" />
+            <span className="sidebarListItemText">Event</span>
+          </li>
+          <li className="sidebarListItem">
+            <Course className="sidebarIcon" />
+            <span className="sidebarListItemText">Course</span>
+          </li>
+        </ul>
+        <button className="sidebarButton">Show More</button>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="sidebarFriendImg"
+            />
+            <span className="sidebarFriendName">John Cena</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="sidebarFriendImg"
+            />
+            <span className="sidebarFriendName">John Cena</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="sidebarFriendImg"
+            />
+            <span className="sidebarFriendName">John Cena</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="sidebarFriendImg"
+            />
+            <span className="sidebarFriendName">John Cena</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import RightBar from "../../components/rightBar/RightBar.jsx";
+import SideBar from "../../components/sideBar/SideBar.jsx";
 import TopBar from "../../components/topBar/TopBar.jsx";
+import Feed from "../../components/feed/Feed.jsx";
+import "./Home.css";
+
 export default function Home() {
   return (
     <div>
       <TopBar />
+      <div className="homeContainer">
+        <SideBar />
+        <Feed />
+        <RightBar />
+      </div>
     </div>
   );
 }
